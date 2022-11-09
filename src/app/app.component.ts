58f8d756a6fe5af81a64fbe40dff3fa1a643adbc
Attinder <Attinderkahlon@gmail.com>
2022-11-09 11:38:36 +1100

    angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'my-first-project';
+  newMemberName = '';
+  members: string[] = [];
+  errorMessage = '';
+
+  onInput(member: string) {
+    this.newMemberName = member;
+  }
+
+  addMember() {
+    if (!this.newMemberName) {
+      this.errorMessage = 'Name can not be empty';
+      return;
+    }
+    this.members.push(this.newMemberName);
+    this.newMemberName = '';
+  }
 }
